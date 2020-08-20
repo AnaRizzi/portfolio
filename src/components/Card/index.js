9112be0bb85eb7ce9fcaec7d395f37e49821a7e1
Ana Rizzi <ana.rizzi.eu@gmail.com>
2020-08-20 12:36:45 -0300

    estrutura da pagina portfolio montada

---
+++ src/components/Card/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './style.css';
+import ImgCard from '../ImgCard';
+import github from '../../assets/img/github.png'
+
+export default function MyCard(props){
+    return(
+    
+        <div className="card">
+            <ImgCard className="card-img-top" src={props.src} alt={props.alt} />
+            <div className="card-body">
+                <h5 className="card-title">{props.titulo}</h5>
+                <p className="card-text">{props.children}</p>
+                
+            </div>
+            <div className="text-center botao">
+                <a href="/portfolio" class="btn btn-sm btn-primary">Detalhes</a>
+                <a href={props.github} target="_blank"> <img src={github} alt="github" className="github" /> </a>
+            </div>
+            <div className="card-footer">
+            <small className="text-muted">{props.linguagens}</small>
+            </div>
+        </div>
+
+    )
+}
--- src/components/pages/Portfolio/index.js
+++ src/components/pages/Portfolio/index.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import Menu from '../../Menu';
 import './style.css';
+import MyCard from '../../Card';
+import niceface from '../../../assets/img/portfolio/nice_face1000x600.png'
 
 
 function Portfolio() {
   return (
     <>
-      <Menu/>
+      <Menu />
       <div className="portfolio">
-Tela portfólio
+        <h1 className="text-center fonteSubtitulo">Conheça meu portfólio</h1>
+        <div className="card-group">
+
+          <div className="col-md-6 col-lg-4 containerCard CSharp">
+            <MyCard src={niceface} alt="Nice Face" titulo="Sistema de gerenciamento de uma clínica estética" linguagens="C# | Asp.net | MVC | MySql" github="https://github.com/AnaRizzi">
+              Sistema desenvolvido em Asp.net MVC com C# e conexão ao banco de dados MySql
+            </MyCard>
+          </div>
+
+          <div className="col-md-6 col-lg-4 containerCard">
+            <MyCard src={niceface} alt="Nice Face" titulo="Título do exemplo" linguagens="C# | Asp.net | MVC | MySql" github="https://github.com/AnaRizzi">
+              Descrição
+            </MyCard>
+          </div>
+
+          <div className="col-md-6 col-lg-4 containerCard">
+            <MyCard src={niceface} alt="Nice Face" titulo="Título do exemplo" linguagens="C# | Asp.net | MVC | MySql" github="https://github.com/AnaRizzi">
+              Descrição
+            </MyCard>
+          </div>
+
+        </div>
       </div>
     </>
   );
