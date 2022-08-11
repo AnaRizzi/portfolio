9c95b75af9d24449f09f136863a3b24200607319
Ana Rizzi <ana.rizzi.eu@gmail.com>
2022-08-11 20:01:07 -0300

    Updates

---
+++ precache-manifest.fa0b83d39e3f3597786125429be718c4.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93a47a09b6912e9c230e99aa6509648f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b4ff1a54bb5de69911ca",
+    "url": "/portfolio/static/css/main.f099ea54.chunk.css"
+  },
+  {
+    "revision": "4e133d959ed76a5b6ef0",
+    "url": "/portfolio/static/js/2.2f029c17.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.2f029c17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4ff1a54bb5de69911ca",
+    "url": "/portfolio/static/js/main.e0ca264e.chunk.js"
+  },
+  {
+    "revision": "b29fcd13dc2ce97e9476",
+    "url": "/portfolio/static/js/runtime-main.c5bad38c.js"
+  },
+  {
+    "revision": "0698ba2369bcdf10d87baabf8936388c",
+    "url": "/portfolio/static/media/anaRizziClaro.0698ba23.png"
+  },
+  {
+    "revision": "7f23c6437fd083212b88405f39bc727b",
+    "url": "/portfolio/static/media/anabnb1000x600.7f23c643.png"
+  },
+  {
+    "revision": "21af21eb1a8d9d3b77bc32525b393cef",
+    "url": "/portfolio/static/media/background.21af21eb.png"
+  },
+  {
+    "revision": "946d0b831a021ac8350c45d8b7617ec9",
+    "url": "/portfolio/static/media/bancos1000x600.946d0b83.png"
+  },
+  {
+    "revision": "6e0ae54276ff6082accbfcb4906ae9f5",
+    "url": "/portfolio/static/media/cep1000x600.6e0ae542.png"
+  },
+  {
+    "revision": "8ba94e9e5fa91cee308946ce9862e14d",
+    "url": "/portfolio/static/media/docesrizzi1000x600.8ba94e9e.png"
+  },
+  {
+    "revision": "21f96c3f52e73ee96ec15cc105c6ffe9",
+    "url": "/portfolio/static/media/email.21f96c3f.png"
+  },
+  {
+    "revision": "bfb9ef3477e88b1e97f3312130dad23a",
+    "url": "/portfolio/static/media/erro.bfb9ef34.jpg"
+  },
+  {
+    "revision": "3b1c31b8ce7a65bcf854d96840a00668",
+    "url": "/portfolio/static/media/hannah1000x600.3b1c31b8.png"
+  },
+  {
+    "revision": "fa8b07ff267cc6bc0c4ef38980ac8aab",
+    "url": "/portfolio/static/media/nice_face1000x600.fa8b07ff.png"
+  },
+  {
+    "revision": "65c0feb7a89c8c5249e35a2979175423",
+    "url": "/portfolio/static/media/portfolio1000x600.65c0feb7.png"
+  },
+  {
+    "revision": "a266fb3105e89bb6f4beff932e0bd11c",
+    "url": "/portfolio/static/media/rabbit1000x600.a266fb31.png"
+  },
+  {
+    "revision": "2b30aece33bdd920c07b4a51de92bc6e",
+    "url": "/portfolio/static/media/receitas1000x600.2b30aece.png"
+  },
+  {
+    "revision": "ce73fe1621c19a9fd004a52f852ec21d",
+    "url": "/portfolio/static/media/sobre.ce73fe16.jpg"
+  }
+]);
